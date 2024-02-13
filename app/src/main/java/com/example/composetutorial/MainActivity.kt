46001bab8e8c58c7316b0c306c7ad713acd8d77e
KyryloMuzychka <kyryll2016@gmail.com>
2024-02-13 18:32:37 +0200

    lab4

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.composetutorial
 
+import SampleData
 import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,14 +17,19 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,9 +40,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTutorialTheme {
-                Surface {
-                    MessageCard(message = Message("Kate", "Hello! How are you?"))
-                }
+                Conversation(messages = SampleData.conversationSample)
             }
         }
     }
@@ -43,47 +50,77 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(message: Message) {
-    Surface(
-            shape = MaterialTheme.shapes.medium,
-            shadowElevation = 5.dp,
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.profile_img),
+            contentDescription = "Contact profile picture",
             modifier = Modifier
-                .padding(all = 8.dp)
-                .width(LocalConfiguration.current.screenWidthDp.dp)) {
-        Row(modifier = Modifier.padding(all = 8.dp)) {
-            Image(
-                painter = painterResource(R.drawable.profile_img),
-                contentDescription = "Contact profile picture",
-                modifier = Modifier
-                    .size(40.dp)
-                    .clip(CircleShape)
-                    .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+                .size(64.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = message.author,
+                style = MaterialTheme.typography.titleSmall,
+                modifier = Modifier.padding(horizontal = 5.dp)
             )
-            Spacer(modifier = Modifier.width(8.dp))
-            Column {
-                Text(
-                    text = message.author,
-                    style = MaterialTheme.typography.titleSmall
-                )
-                Spacer(modifier = Modifier.height(4.dp))
+            Spacer(modifier = Modifier.height(4.dp))
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 3.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(5.dp)
+            ) {
                 Text(
                     text = message.body,
-                    style = MaterialTheme.typography.bodyMedium
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(8.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1
                 )
             }
         }
     }
 }
 
-@Preview(name = "Light Mode")
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message = message)
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    name = "Light Mode"
+)
 @Preview(
     uiMode = Configuration.UI_MODE_NIGHT_YES,
     showBackground = true,
-    name = "Dark Mode")
+    name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard() {
     ComposeTutorialTheme {
         Surface {
             MessageCard(Message("Anna", "It's Jetpack Compose"))
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewConversation() {
+    ComposeTutorialTheme {
+        Conversation(messages = SampleData.conversationSample)
     }
 }
---
+++ app/src/main/java/com/example/composetutorial/SampleData.kt
@@ -0,0 +1,75 @@
+import com.example.composetutorial.Message
+
+/**
+ * SampleData for Jetpack Compose Tutorial 
+ */
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Kate",
+            "Test...Test...Test..."
+        ),
+        Message(
+            "Kate",
+            """List of Android versions:
+            |Android KitKat (API 19)
+            |Android Lollipop (API 21)
+            |Android Marshmallow (API 23)
+            |Android Nougat (API 24)
+            |Android Oreo (API 26)
+            |Android Pie (API 28)
+            |Android 10 (API 29)
+            |Android 11 (API 30)
+            |Android 12 (API 31)""".trim()
+        ),
+        Message(
+            "Kate",
+            """I think Kotlin is my favorite programming language.
+            |It's so much fun!""".trim()
+        ),
+        Message(
+            "Kate",
+            "Searching for alternatives to XML layouts..."
+        ),
+        Message(
+            "Kate",
+            """Hey, take a look at Jetpack Compose, it's great!
+            |It's the Android's modern toolkit for building native UI.
+            |It simplifies and accelerates UI development on Android.
+            |Less code, powerful tools, and intuitive Kotlin APIs :)""".trim()
+        ),
+        Message(
+            "Kate",
+            "It's available from API 21+ :)"
+        ),
+        Message(
+            "Kate",
+            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+        ),
+        Message(
+            "Kate",
+            "Android Studio next version's name is Arctic Fox"
+        ),
+        Message(
+            "Kate",
+            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+        ),
+        Message(
+            "Kate",
+            "I didn't know you can now run the emulator directly from Android Studio"
+        ),
+        Message(
+            "Kate",
+            "Compose Previews are great to check quickly how a composable layout looks like"
+        ),
+        Message(
+            "Kate",
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        Message(
+            "Kate",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+    )
+}
